d3cff1120aff2abb6f5097f11ef8ca1cec588519
Valentina Sinitsyna <v-sin@yandex.ru>
2019-07-05 16:21:00 +0300

    changes in main file
    3 blocks for the programm
    examples to read & save samples
    just as training exercise

--- functions/smoothing_filter.m
+++ functions/smoothing_filter.m
@@ -4,5 +4,7 @@ se90=strel('line', 3, 90);
 se0=strel('line', 3, 0);
 BWsdil=imdilate(BW, [se90 se0]);
 BWdfill=imfill(BWsdil, 'holes');
-k=BWdfill;
+BWsdil2=imdilate(BWdfill, [se90 se0]);
+BWdfill2=imdilate(BWsdil2, [se90 se0]);
+k=BWdfill2;
 end
--- main.m
+++ main.m
@@ -1,13 +1,24 @@
+%% block 1 - cutting images
 clc
 clear
-image_full = imread('resources/135320_allgrains.png');
-% image_full= image_ful(500:1500,3000:4000);
+image_ful= imread('resources/135320_allgrains.png');
+image_full= image_ful(500:1500,2000:4164);
 samples  = image_cut(image_full);
-
+% block 2 - examples for using my functions and code for saving samples to
+% files
 for i=1:size(samples,2)
-  saved_samples = ['resources/' num2str(i+16+12+10+13+46+68+33) '.jpg']; 
-  imwrite(samples{i}, saved_samples);
+  extention_samples{i}=extention_sample(samples{i},10);
+  smoothing_samples{i} = smoothing_filter(extention_samples{i})
+  saved_samples = ['resources/smoothing/' num2str(i) '.jpg']; 
+  imwrite(smoothing_samples{i}, saved_samples);
 end
-
- %TODO add filters for samples: delete small ones
- %TODO add function for deleting border samples    
+ %% block 3 - code for using saved images +  functions for making smoothing samples and saving them to new file 
+for i=1:198
+    saved_samples = ['resources/' num2str(i) '.jpg']; 
+    samples{i} = imread(saved_samples); 
+    extention_samples{i}=extention_sample(samples{i},10);
+    smoothing_samples{i} = smoothing_filter(extention_samples{i});
+    saved_samples1 = ['resources/smoothing/sm' num2str(i) '.jpg']; 
+   
+    imwrite(smoothing_samples{i}, saved_samples1);
+end
