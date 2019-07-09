d6b632895333a421bc65c496b7ee449168f078eb
Valentina Sinitsyna <v-sin@yandex.ru>
2019-07-09 22:27:43 +0300

    elongation function
    there is only necessery code

--- functions/elongation_of_image.m
+++ functions/elongation_of_image.m
@@ -1,13 +1,9 @@
-function [k,aa, bb] = elongation_of_image (image);
+function [k] = elongation_of_image (image);
 d = regionprops(image, 'Orientation');
 degree= struct2array(d);
-figure, bb=imshow(image);
 new_image = imrotate(image,degree, 'bilinear');
-figure, aa=imshow(new_image);
 I=size(image,1);
 J=size(image,2);
-length_i=0;
-length_j=0;
 for i=1:I
       a(i)=sum(image(i,:)==1);
 end
--- main.m
+++ main.m
@@ -7,6 +7,6 @@ samples = image_cut(image_full);
 for i=1:size(samples, 2)
  extention_samples{i}=extention_sample(samples{i},10);
  smoothing_samples{i} = smoothing_filter(extention_samples{i});
- [elongation(i),j(i),k(i)]=elongation_of_image(smoothing_samples{i});
+ elongation(i)=elongation_of_image(smoothing_samples{i});
  form_factorr(i)=form_factor(extention_samples{i});
 end
