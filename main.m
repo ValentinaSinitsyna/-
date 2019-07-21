80f571a1ff07f60a9e2ec1b14a52e69bbbd50b0e
Mikle Ku <miklekudryavtsev@MacBook-Air-Mikle-34.local>
2019-07-21 15:31:54 +0300

    fill_sample without recursion
    recursion is deleted
    slow std function find() is replaced with find_first()

--- main.m
+++ main.m
@@ -2,8 +2,9 @@ clc
 clear
 I= imread('resources/135320_allgrains.png');  
 image_full= no_interference(I);
-image_full=image_full(1:500, 1:500);
+%image_full=image_full(1:500, 1:500);
 samples = image_cut(image_full);
+
 for i=1:size(samples, 2)   
  extention_samples{i}=extention_sample(samples{i},10);
  smoothing_samples{i} = smoothing_filter(extention_samples{i});
