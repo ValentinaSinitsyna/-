fa1335c00b2b2fef72aa6181e629fa9f59987ffd
Valentina Sinitsyna <v-sin@yandex.ru>
2019-07-22 21:58:05 +0300

    new elongation function
    changes in code
    no errors

--- functions/elongation_of_image.m
+++ functions/elongation_of_image.m
@@ -1,15 +1,12 @@
-function [k] = elongation_of_image (image);
+function [length_i, length_j,k, degree] = elongation_of_image (image);
 d = regionprops(image, 'Orientation');
 degree= struct2array(d);
-new_image = imrotate(image,degree, 'bilinear');
-I=size(image,1);
-J=size(image,2);
-for i=1:I
-      a(i)=sum(image(i,:)==1);
-end
-length_i=max(a(:));
+new_image = imrotate(image,degree,'bilinear');
+x=regionprops(new_image,'MajorAxisLength');
+length_i = struct2array(x);
+J=size(new_image,2);
 for j=1:J
-      b(j)=sum(image(:,j)==1);
+      b(j)=sum(new_image(:,j)==1);
 end
   length_j=max(b(:));  
   k=length_i/length_j;
--- main.m
+++ main.m
@@ -1,16 +1,16 @@
 clc
 clear
-I= imread('resources/135320_allgrains.png');  
-image_full= no_interference(I);
-image_full=image_full(1:500, 1:500);
+IIj=imread('resources/8.png');
+image_full= no_interference(IIj);
+image_full=image_full(1:500,1:500);
 samples = image_cut(image_full);
 for i=1:size(samples, 2)   
  extention_samples{i}=extention_sample(samples{i},10);
  smoothing_samples{i} = smoothing_filter(extention_samples{i});
- elongation(i)=elongation_of_image(smoothing_samples{i});
- II(i)=size(smoothing_samples{1,i},1);
- J(i)=size(smoothing_samples{1,i},2);
- [f_f(i), length(i), square(i)]=form_factor(extention_samples{i});
+ [II(i),J(i), elongation(i)]=elongation_of_image(smoothing_samples{i});
+%   imshow(smoothing_samples{i})
+%   figure
+ [f_f(i), length(i), square(i)]=form_factor(smoothing_samples{i});
  results(i,1)=i;
  results(i,2)=II(i);
  results(i,3)=J(i);
@@ -18,6 +18,15 @@ for i=1:size(samples, 2)
  results(i,5)=square(i);
  results(i,6)=f_f(i);
  results(i,7)=elongation(i);
- T=array2table(results, 'VariableNames', {'number_of_image','number_of_rows','number_of_columns' , 'length_of_line','square_of_image' , 'form_factor' ,'elongation_of_image'}); 
+ T=array2table(results, 'VariableNames', {'number_of_image','length','width' , 'length_of_line','square_of_image' , 'form_factor' ,'elongation_of_image'}); 
 end
-writetable(T,'myData.csv');
+writetable(T,'myData.xlsx');
+% for i=1:size(samples,2)
+%   saved_samples = ['resources/' num2str(i+16+12+10+13+46+68+33) '.jpg']; 
+%   imwrite(samples{i}, saved_samples);
+% % end
+imshow(smoothing_samples{6});
+%%
+clc
+A=[0,0,1,0,0,0;0,0,1,0,0,0;0,1,1,1,0,0;0,0,1,0,0,0;0,0,1,0,0,0;0,0,1,0,0,0]
+[II,J, elongation, d]=elongation_of_image(A)
